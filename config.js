96b773d424f45d5d0cdeede9e4796000bd562e59
VanshVala <102245923+Vanshvala23@users.noreply.github.com>
2024-02-14 20:09:24 +0530

    Delete config.js

--- config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    token:"MTE5NDI4MzY1MTczMTU2MjUxNg.G_76fe.Mkm64YZIaY5sLMOWy8CdTOVkX1ukDnp7Ds4fb4",
-    clientId: "1194283651731562516",
-    guildId: "1127487023415046284",
-};
